89cca460b95b8c793640e40a79990ab43b5b7747
ANURAG SINGH <74447827+anuragsinghok@users.noreply.github.com>
2020-11-30 21:19:09 +0530

    Add files via upload

---
+++ AtoZ.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int i;
+    for( i='A'; i<='Z'; i++) //check this and understand again from net do it first then move learn this first
+    
+    {
+        printf("%c ",i);
+
+    
+    }
+    getch();
+}
